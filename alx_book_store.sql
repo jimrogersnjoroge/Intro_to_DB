c3ca1f53eb1d06979f11d2fe0704a1634cbc57d9
jimrogersnjoroge <168189915+jimrogersnjoroge@users.noreply.github.com>
2024-07-27 15:22:51 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,47 +1,42 @@
--- Create the database if it doesn't already exist
-CREATE DATABASE IF NOT EXISTS alx_book_store;
+CREATE DATABASE alx_book_store;
 
--- Use the database
 USE alx_book_store;
 
--- Create the Authors table
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+CREATE TABLE Authors (
+  author_id INT PRIMARY KEY AUTO_INCREMENT,
+  author_name VARCHAR(215) NOT NULL
 );
 
--- Create the Books table
-CREATE TABLE IF NOT EXISTS books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+CREATE TABLE Books (
+  book_id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(130) NOT NULL,
+  author_id INT,
+  price DOUBLE,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Create the Customers table
-CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL
+CREATE TABLE Customers (
+  customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215) UNIQUE,
+  address TEXT
 );
 
--- Create the Orders table
-CREATE TABLE IF NOT EXISTS orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
+CREATE TABLE Orders (
+  order_id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_id INT,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)   
+
 );
 
--- Create the Order_Details table
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
+CREATE TABLE Order_Details   
+ (
+  orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
