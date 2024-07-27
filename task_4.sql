089749735fe5bd9a08665d05639b85cb6709ac7e
jimrogersnjoroge <168189915+jimrogersnjoroge@users.noreply.github.com>
2024-07-27 15:57:54 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,13 @@
--- task_4.sql
-
+-- Query the information_schema to get the table description
 SELECT 
-  COLUMN_NAME, 
-  COLUMN_TYPE, 
-  CHARACTER_MAXIMUM_LENGTH, 
-  NUMERIC_PRECISION, 
-  NUMERIC_SCALE, 
-  COLUMN_DEFAULT, 
-  IS_NULLABLE, 
-  COLUMN_KEY, 
-  EXTRA
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_KEY AS 'Column Key',
+    EXTRA AS 'Extra Information'
 FROM 
-  INFORMATION_SCHEMA.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
-  TABLE_SCHEMA = 'alx_book_store' 
-  AND TABLE_NAME = 'books';
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
+
